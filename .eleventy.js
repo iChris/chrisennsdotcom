5b9db63c2197bed3b805ea16c955b4a30c456371
Chris Enns <chris.enns@gmail.com>
2020-02-11 22:58:58 -0600

    fixed permalinks

--- .eleventy.js
+++ .eleventy.js
@@ -28,6 +28,19 @@ module.exports = function(config) {
   config.addTransform('htmlmin', htmlMinTransform);
   config.addTransform('parse', parseTransform);
 
+  // Permalink possible fix from https://github.com/11ty/eleventy/issues/923
+  //   "permalink": "{{ page | post_permalink }}"
+config.addFilter("post_permalink", page => {
+  const yyyy = page.date.getFullYear();
+  const mm = String(page.date.getMonth() + 1).padStart(2, "0");
+  const slug = page.fileSlug.replace(/^\d{4}-\d{2}-\d{2}\./, "");
+  return `${yyyy}/${mm}/${slug}/`;
+});
+
+config.addFilter('dateFilter', dateFilter);
+config.addFilter('markdownFilter', markdownFilter);
+config.addFilter('w3DateFilter', w3DateFilter);
+
   // Passthrough copy
   config.addPassthroughCopy('src/fonts');
   config.addPassthroughCopy('src/images');
@@ -77,31 +90,5 @@ module.exports = function(config) {
       output: 'dist'
     },
     passthroughFileCopy: true
-  };
-};
-
-module.exports = eleventyConfig => {
-  // Option 1: Separate filters for year/month.
-  // USAGE:
-  //   "permalink": "/{{ page.date | permalink_year }}/{{ page.date | permalink_month }}/{{ page.fileSlug }}/"
-  eleventyConfig.addFilter("permalink_year", dateObj => dateObj.getFullYear());
-  eleventyConfig.addFilter("permalink_month", date =>
-    String(date.getMonth() + 1).padStart(2, "0")
-  );
-
-  // Option 2: Single filter for year/month/slug.
-  // USAGE:
-  //   "permalink": "{{ page | post_permalink }}"
-  eleventyConfig.addFilter("post_permalink", page => {
-    const yyyy = page.date.getFullYear();
-    const mm = String(page.date.getMonth() + 1).padStart(2, "0");
-    return `${yyyy}/${mm}/${page.fileSlug}/`;
-  });
-
-  return {
-    dir: {
-      input: "src",
-      output: "www"
-    }
   };
 };
