3daf4a93f3951d3c506ed01b1ba61992d5c2cbb7
Chris Enns <chris.enns@gmail.com>
2022-07-11 15:04:27 -0600

    wtf

--- .eleventy.js
+++ .eleventy.js
@@ -38,7 +38,21 @@ module.exports = function(config) {
   // set the library to process markdown files
   config.setLibrary("md", markdownLib);
 
-  
+  config.addFilter('stripIndex', function(path) {
+    return path.replace('/index.html', '/');
+  })
+
+  config.addFilter("isoDateOnly", function(date) {
+    var d = new Date(date),
+        month = '' + (d.getMonth() + 1),
+        day = '' + d.getDate(),
+        year = d.getFullYear();
+
+    if (month.length < 2) month = '0' + month;
+    if (day.length < 2) day = '0' + day;
+
+    return [year, month, day].join('-');
+  })
 
   // Layout aliases
   config.addLayoutAlias('home', 'layouts/home.njk');
