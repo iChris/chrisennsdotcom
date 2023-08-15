6177a2d5e52d74e0c172c1ca2ade635549089872
Brett Nelson <brett@wipdeveloper.com>
2023-08-15 17:36:26 -0500

    some date updates

--- src/filters/w3-date-filter.js
+++ src/filters/w3-date-filter.js
@@ -1,5 +1,15 @@
-module.exports = function w3cDate(value) {
-  const dateObject = new Date(value);
+module.exports = function w3cDate(date) {
+  let dateObject = new Date(date);
+
+  // If the date is invalid, try to parse it from the file slug
+  if (dateObject instanceof Date && isNaN(dateObject)) {
+    // Some of the dates are the file slug passed in
+    if (date.includes('.')) {
+      date = date.split('.')[0];
+
+      dateObject = new Date(date);
+    }
+  }
 
   return dateObject.toISOString();
 };
